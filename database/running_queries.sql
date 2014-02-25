aa8c54f47fa5d15adb65b3af4c4869351ad7f55e
Tom Kimani <tosh0948@gmail.com>
2014-02-25 12:44:22 +0300

    Running sql statements

--- database/running_queries.sql
+++ database/running_queries.sql
@@ -3,11 +3,15 @@
 /*--Update Transactions--
 update transactions set isRecorded='1' where isRecorded='0'
 
-update Allocation set deallocationDate= GETDATE(), deallocatedBy='10' where allocationId='83'
+update Allocation set deallocationDate= GETDATE(), deallocatedBy='10' where allocationId='88'
 */
 
 /*==Latest Transactions  */
-select * from transactions where isRecorded='0'
+select * from transactions where clCode='pb/02495' and transaction_date='2014-02-24'
+
+select * from Client where clcode='PB/02564'
+
+update Client set phone='0721797962' where clcode='PA/00197' 
 
 /*---Allocations ---*/
 select * from Allocation
